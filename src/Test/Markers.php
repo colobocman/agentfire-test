b480f83993fc406720e82fe11eea3efb6777faf6
Roman Prokopyshyn <colobocman@gmail.com>
2021-01-12 10:30:00 +0200

    add layout descriptions

--- src/Test/Markers.php
+++ src/Test/Markers.php
@@ -54,16 +54,28 @@ class Markers {
 			$lat = get_field('lat');
 			$lng = get_field('lon');
 
+			if( is_user_logged_in() && is_author(get_current_user_id()) ) {
+				$marker_color = "#ff8888";
+			} else {
+				$marker_color = "#ff88ff";
+			}
+
 			$properties = array(
 				"title"=> $name,
-			    "marker-symbol" => "star",
-		        "marker-color" => "#ff8888",
+		        "marker-color" => $marker_color,
 		    );
+		    $date = get_the_date('Y-m-d H:i:s');
+		    $properties['description'] = $date;
+
 			$tags = get_the_terms( get_the_ID(), 'marker_tag');
 			if ((!is_wp_error( $tags )) and ($tags !== false)) { 
+				$tag_names  = array();
 				foreach ($tags as $tag) {
 					$properties[$tag->slug] = true;
+					$tag_names[] = $tag->name; 
 				}
+				$properties['description'] .=  "<br><i>".implode( ', ', $tag_names ).'</i>';
+
 			}
 
 			$data[] = array(
